9e5dd949e10797cd6fe42c173bc74ceeb2b9e2d8
FR1530 <148667398+FR1530@users.noreply.github.com>
2023-10-22 03:15:15 -0300

    conectada a la base de datos

--- MAIN.js
+++ MAIN.js
@@ -20,21 +20,33 @@ con.connect(function(err) {
 
 
 
+function pedir_tabla(table_name,res){
+
+  con.query("SELECT * FROM " + table_name, function (err, result) {
+    if (err) throw err;
+    console.log(JSON.stringify(result))
+    res.json(result);
+  });
+}
+
+
 
 
 app.get('/index.js', (req, res) => {res.sendFile("index.js",{root:__dirname})});
 app.get('/index.css', (req, res) => {res.sendFile("index.css",{root:__dirname})});
 
+app.get('/tables/productos', (req, res) => {
+  pedir_tabla("productos",res)
+})
 
 
 app.get('/', (req, res) => {
 
+
+
+
     console.log("Connected!");
-    con.query("SELECT * FROM productos", function (err, result) {
-      if (err) throw err;
-      //console.log("Result: " + result);
-      res.sendFile("index.html",{root:__dirname})
-    });
+    res.sendFile("index.html",{root:__dirname})
 
 
   //res.send('Hello World!')
